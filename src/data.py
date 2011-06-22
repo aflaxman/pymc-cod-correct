c992c62e8a218dfecee4a16a34595fd26f2e70d9
l dwyerlindgren <ladwyer@uw.edu>
2011-06-21 19:58:22 -0700

    Added function to simulate cause fractions from J causes Added function to plot two causes from a simulate data set

--- src/data.py
+++ src/data.py
@@ -3,6 +3,7 @@
 
 import pylab as pl
 import pymc as mc
+import numpy as np
 
 # simple models for some non-uniformly distributed subsets of the plane
 def simulate_data(N):
@@ -12,3 +13,23 @@ def simulate_data(N):
     X = [mc.rnormal(true_csmf[0], tau[0], size=N),
          mc.rnormal(true_csmf[1], tau[1], size=N)]  # TODO: find a cooler way to do this
     return pl.array(X).T
+	
+def sim_data(N,true_csmf,true_csmf_sd):
+	""" 
+	Create an NxJ matrix of simulated data (J is determined by the length 
+	  of true_csmf). 
+	
+	true_csmf - a list of true cause fractions (must sum to one)
+	true_csmf_sd - a list of the standard deviations corresponding to the true csmf's
+	"""
+	assert sum(true_csmf)==1, 'The sum of elements of true_csmf must equal 1' 
+	assert len(true_csmf)==len(true_csmf_sd), 'The length of true_csmf and true_csmf_sd must be the same'
+	X = []
+	for i in range(len(true_csmf)):
+		x = mc.rnormal(mu=true_csmf[i], tau=true_csmf_sd[i]**-2, size=N) # this can (does) give cause fractions outside [0,1]
+		if i == 0: 
+			X = x
+		else: 
+			X = np.vstack((X,x))
+	return X.T
+	
--- src/graphics.py
+++ src/graphics.py
@@ -1,5 +1,20 @@
 import networkx as nx
 import pylab as pl
+import numpy as np
 
 # put messy matplotlib code here so I don't have to look at it if I
 # don't want to
+
+# plot of simulated data
+def plot_sim_data(X, index=[0,1]): 
+	if np.concatenate(X).max() > 1 or np.concatenate(X).min() < 0: 
+		print 'Warning: data outside [0,1] is not shown'
+	pl.clf()
+	pl.axis([0,1,0,1])
+	pl.xlabel('csmf (cause %d)' %(index[0]+1))
+	pl.ylabel('csmf (cause %d)' %(index[1]+1))	
+	pl.plot(X[:,index[0]], X[:,index[1]], 'g.')
+	if np.shape(X)[1]==2: 
+		pl.plot([1,0],[0,1])
+	
+	
