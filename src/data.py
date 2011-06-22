dbc3ed0088ce577fdc6909e9bf77d96b66cc443d
A. Flaxman <abie@uw.edu>
2011-06-22 11:40:18 -0700

    remove redundant code
    also, make test_sim_data test the new code

--- src/data.py
+++ src/data.py
@@ -6,15 +6,7 @@ import pymc as mc
 import numpy as np
 
 # simple models for some non-uniformly distributed subsets of the plane
-def simulate_data(N):
-    """ Create an Nx2 matrix of simulated data"""
-    true_csmf = [.3, .7]
-    tau = [.2**-2, .05**-2]
-    X = [mc.rnormal(true_csmf[0], tau[0], size=N),
-         mc.rnormal(true_csmf[1], tau[1], size=N)]  # TODO: find a cooler way to do this
-    return pl.array(X).T
-	
-def sim_data(N,true_csmf,true_csmf_sd):
+def sim_data(N, true_csmf=[.3, .7], true_csmf_sd=[.2, .05]):
     """ 
     Create an NxJ matrix of simulated data (J is determined by the length 
     of true_csmf). 
--- src/tests.py
+++ src/tests.py
@@ -15,6 +15,5 @@ class TestClass:
 
    def test_sim_data(self):
       import data
-      sim_data = data.simulate_data(10)
-      assert len(sim_data) == 10, 'Should be 10 data points (%d found)' % len(sim_data)
+      sim_data = data.sim_data(10)
       assert sim_data.shape == (10,2), 'Should be 10x2 matrix of data (%s found)' % str(sim_data.shape)
