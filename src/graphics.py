a7b21b6fcdb24edc0b071b926a439ce8df53e135
l dwyerlindgren <ladwyer@uw.edu>
2011-06-23 16:21:26 -0700

    changed plot_all_sim_data to show only one version of each combination of causes. Still need to get rid of excess margin space and such, but will come back to that

--- src/graphics.py
+++ src/graphics.py
@@ -25,15 +25,14 @@ def plot_all_sim_data(X):
     pl.clf()
     J = np.shape(X)[1]
     for i in range(J): 
-        for j in range(J):
+        for j in range(i+1,J):
             # plot for upper triangle
-            p = J*i+j+1
-            pl.subplot(J,J,p)
+            p = (J-1)*i+j
+            pl.subplot(J-1,J-1,p)
             pl.plot(X[:,j], X[:,i], 'g.')
             pl.axis([0,1,0,1])
-            if J == 2 and i != j: 
+            if J == 2: 
                 pl.plot([1,0],[0,1])
-            if j == 0:
+            if j == i+1:
                 pl.ylabel('csmf (cause %d)' %(i+1))
-            if i == (J-1):
                 pl.xlabel('csmf (cause %d)' %(j+1))
