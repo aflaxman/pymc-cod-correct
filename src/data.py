fd9b3dd93741868cccc42d00229d2b0d1683e25c
A. Flaxman <abie@uw.edu>
2011-06-23 12:27:56 -0700

    simplify code for data simulation

--- src/data.py
+++ src/data.py
@@ -4,14 +4,6 @@
 import pylab as pl
 import pymc as mc
 import numpy as np
-from math import log, exp
-
-# logit and inverse logit functions 
-def logit(x):
-    return log(x/(1-x))
-
-def invlogit(x):
-    return exp(x)/(1+exp(x))
 
 # simple models for some non-uniformly distributed subsets of the plane
 def sim_data(N, true_csmf=[.3, .7], true_csmf_sd=[.2, .05]):
@@ -23,29 +15,22 @@ def sim_data(N, true_csmf=[.3, .7], true_csmf_sd=[.2, .05]):
     true_csmf_sd - a list of the standard deviations corresponding to the true csmf's
     """
 
-    assert sum(true_csmf)==1, 'The sum of elements of true_csmf must equal 1' 
+    assert pl.allclose(sum(true_csmf), 1), 'The sum of elements of true_csmf must equal 1' 
     assert len(true_csmf)==len(true_csmf_sd), 'The length of true_csmf and true_csmf_sd must be the same'
+    J = len(true_csmf)
 
     ## transform the mean and sd to logit space 
-    transformed_csmf = [logit(i) for i in true_csmf]
+    transformed_csmf = mc.logit(true_csmf)
     transformed_var = []
-    for i in range(len(true_csmf)):
+    for pi_i, sigma_pi_i in zip(true_csmf, true_csmf_sd):
         # TODO: verify that this actually the appropriate equation; the standard deviation of the data returned doesn't match what is being provided in the arguments
-        transformed_var.append( (1/(true_csmf[1]*(1-true_csmf[1])))**2 * true_csmf_sd[i]**2 )
+        transformed_var.append( (1/(pi_i*(1-pi_i)))**2 * sigma_pi_i**2 )
 
-    ## draw from a normal distribution 
-    X = []
-    for i in range(len(transformed_csmf)):
-        x = mc.rnormal(mu=transformed_csmf[i], tau=transformed_var[i]**-1, size=N)
-        if i == 0: 
-            X = x
-        else: 
-            X = np.vstack((X,x))
+    ## draw from distribution 
+    X = mc.rnormal(mu=transformed_csmf, tau=pl.array(transformed_var)**-1, size=(N,J))
 
     ## back transform the simulated values
-    Y = np.ones(np.shape(X))
-    for i in range(np.shape(X)[0]):
-        for j in range(np.shape(X)[1]):
-            Y[i,j] = invlogit(X[i,j])
-    return Y.T
+    Y = mc.invlogit(X)
+
+    return Y
 
