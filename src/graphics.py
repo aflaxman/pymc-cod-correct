ad09a617a9fcdd70fc291d6650848400f66346f9
l dwyerlindgren <ladwyer@uw.edu>
2011-06-22 21:47:56 -0700

    added plot_all_sim_data to graphics.py to produce lattice style graphs of simulated data for an indenfinite number of causes

--- src/graphics.py
+++ src/graphics.py
@@ -16,5 +16,24 @@ def plot_sim_data(X, i=0, j=1):
     if np.shape(X)[1]==2: 
         pl.plot([1,0],[0,1])
     pl.axis([0,1,0,1])
-	
-	
+
+
+# lattice style plot of simulated data
+def plot_all_sim_data(X): 
+    if np.concatenate(X).max() > 1 or np.concatenate(X).min() < 0: 
+        print 'Warning: data outside [0,1] is not shown'
+    pl.clf()
+    J = np.shape(X)[1]
+    for i in range(J): 
+        for j in range(J):
+            # plot for upper triangle
+            p = J*i+j+1
+            pl.subplot(J,J,p)
+            pl.plot(X[:,j], X[:,i], 'g.')
+            pl.axis([0,1,0,1])
+            if J == 2 and i != j: 
+                pl.plot([1,0],[0,1])
+            if j == 0:
+                pl.ylabel('csmf (cause %d)' %(i+1))
+            if i == (J-1):
+                pl.xlabel('csmf (cause %d)' %(j+1))
