03f82acc6ae04fc2fe41427a96f239b8ef24bf95
l dwyerlindgren <ladwyer@uw.edu>
2011-06-28 14:45:46 -0700

    correction in fit_models.py to filenames to specify both country and year so as to not overwrite files created earlier in the loop and removal of unnecessary code that I meant to remove before the last commit

--- src/fit_models.py
+++ src/fit_models.py
@@ -20,16 +20,14 @@ for iso in isos:
             for year in years: 
             
                 year = str(year)
-                dir = 'C:/Users/ladwyer/pymc-cod-correct/data/%s_%s_%s_%s' % (iso, sex, age, year) 
-                os.mkdir(dir)
-                
+               
                 cf = data.get_cod_data(level=1, keep_age = age, keep_iso3 = iso, keep_sex = sex, keep_year= year)
                 X = data.sim_cod_data(1000, cf)
-                pl.rec2csv(pl.np.core.records.fromarrays(X.T), '../data/sim_cod_data_%s.csv' % (iso))
+                pl.rec2csv(pl.np.core.records.fromarrays(X.T), '../data/sim_cod_data_%s_%s.csv' % (iso, year))
                 
                 bad_model = models.bad_model(X) 
-                pl.rec2csv(pl.np.core.records.fromarrays(bad_model.T), '../data/bad_model_%s.csv' % (iso))
+                pl.rec2csv(pl.np.core.records.fromarrays(bad_model.T), '../data/bad_model_%s_%s.csv' % (iso, year))
 
-                latent_dirichlet = models.fit_latent_dirichlet(X, 1000, 500, 5)
-                pl.rec2csv(pl.np.core.records.fromarrays(latent_dirichlet.T), '../data/latent_dirichlet_%s.csv' % (iso))
+                latent_dirichlet = models.fit_latent_dirichlet(X, 100, 50, 5)
+                pl.rec2csv(pl.np.core.records.fromarrays(latent_dirichlet.T), '../data/latent_dirichlet_%s_%s.csv' % (iso, year))
 
